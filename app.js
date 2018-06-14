9d2194935f08db78e3c2d1cd308659d3cf5f0de6
Anže Fajfar <a.fajfar95@gmail.com>
2018-06-14 10:56:44 +0200

    Ponovno dodan log file ker sem ga ponesreči zbrisal

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 
 // variabile
 let dirCount = [];
+let update;
 
 // uporaba bodyparserja za JSON
 app.use(bodyParser.json());
@@ -24,14 +25,24 @@ app.use(function (req, res, next) {
 app.get('/api/:index', (req, res) => {
     // Variabile
     let index = req.params.index;
-    
+    let d = new Date();
+    let n = d.toISOString();
     // Try block za testiranje ali file s tem indexom obstaja
     try {
         let data = fs.readFileSync('./JSON-data/'+ index + '.json', 'utf8');
         let jsonData = JSON.parse(data);
+        // Zapis za log dodan
+        update  =    "File retrieved with an index of: "
+                    + index + " , " + 
+                    ". Date: " + n;
+        fs.appendFileSync('./log/input.log', update + "\n", 'utf8');
         res.send(jsonData);
     }
     catch(err) {
+        update  =   "Tried to retrieve a file with an index of: "
+                + index + ". File not found." + 
+                " Date: " + n;
+        fs.appendFileSync('./log/input.log', update + "\n", 'utf8');
         res.send(err);
     }
 
@@ -44,13 +55,19 @@ app.post('/api/add', (req, res) => {
     fs.readdirSync('./JSON-data/').forEach( file => {
         dirCount.push(file);
     });
-
+    // Variabile
+    let d = new Date();
+    let n = d.toISOString();
     let respond;
     // Index oz. ime datoteke
     let saveIndex = dirCount.length + 1;
 
     // Zapis file v mapo JSON-data
     let data = fs.writeFileSync('./JSON-data/' + saveIndex + '.json', JSON.stringify(req.body), 'utf8');
+    update  =   "New index file was added. The index of the new file: "
+                + saveIndex + " , " + 
+                ". Date: " + n;
+    fs.appendFileSync('./log/input.log', update + "\n", 'utf8');
     // Še pošiljanje imena oz. indexa datoteke nazaj 
     res.send(
         respond = {
